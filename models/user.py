5e5e6c5310ac697513128e383965241aa58c0462
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-02-16 16:03:42 +0100

    fix user script

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ holds class User"""
-import hashlib
 import models
 from models.base_model import BaseModel, Base
+import hashlib
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
@@ -17,16 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship(
-            "Place",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
-        reviews = relationship(
-            "Review",
-            cascade="all, delete, delete-orphan",
-            backref="user"
-        )
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
@@ -37,11 +29,12 @@ class User(BaseModel, Base):
         """initializes user"""
         super().__init__(*args, **kwargs)
 
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Sets an attribute of this class to a given value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                m = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        """Hashes a user password with MD5"""
+        encryption = hashlib.md5(password.encode())
+        self._password = encryption.hexdigest()
