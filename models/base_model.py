5ffce164049dd93545bd6407df32cb786e39fc31
Chineze ogugua <106745705+Chinabel@users.noreply.github.com>
2023-02-16 16:02:41 +0100

    fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,11 @@
 """
 Contains class BaseModel
 """
-import inspect
+
 from datetime import datetime
 import models
 from os import getenv
+import hashlib
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -58,7 +59,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, password=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -66,16 +67,9 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        frame = inspect.currentframe().f_back
-        func_name = frame.f_code.co_name
-        class_name = ''
-        if 'self' in frame.f_locals:
-            class_name = frame.f_locals["self"].__class__.__name__
-        is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
-        if 'password' in new_dict and not is_fs_writing:
-            del new_dict['password']
+        new_dict.pop('_sa_instance_state', None)
+        if not password:
+            new_dict.pop('password', None)
         return new_dict
 
     def delete(self):
